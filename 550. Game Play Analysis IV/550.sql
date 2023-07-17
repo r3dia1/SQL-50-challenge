17825a7fa27bda29ad2a958c911885a15e40e145
r3dia1 <plplplplp30213@gmail.com>
2023-07-17 15:48:31 +0800

    sql-550

---
+++ 550. Game Play Analysis IV/550.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT ROUND(COUNT(player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) as fraction
+FROM Activity
+WHERE (player_id, event_date) IN
+(SELECT player_id, DATE_ADD(MIN(event_date), INTERVAL 1 DAY) FROM Activity GROUP BY player_id)
